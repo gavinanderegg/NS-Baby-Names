08176b6ca23acc32d7c12d6e8b91be99cb7868ca
Gavin Anderegg <gavin@anderegg.ca>
2017-02-23 11:16:58 -0400

    Project file included + current dir handling

--- test.R
+++ test.R
@@ -1,9 +1,10 @@
 library(readr)
 
 
-babynames <- read_csv("~/Sites/nsbabynames/NS_Top_Twenty_Baby_Names_-_1920-2016.csv", col_types = cols(
+babynames <- read_csv(paste0(getwd(), "/NS_Top_Twenty_Baby_Names_-_1920-2016.csv"), col_types = cols(
   sex = col_factor(levels = c("M", "F")),
   year = col_datetime(format = "%m/%d/%Y %H:%M:%S %p"),
   count = col_integer()))
 
 plot(babynames[['count']], format(babynames[['year']],'%Y'))
+
