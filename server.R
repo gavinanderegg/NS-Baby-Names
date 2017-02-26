f5228e722d87919d7a23bb5124ad9ed94082dcb1
Gavin Anderegg <gavin@anderegg.ca>
2017-02-26 10:50:10 -0400

    Slider using available years

--- server.R
+++ server.R
@@ -1,11 +1,4 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Data from:
 # https://data.novascotia.ca/Population-and-Demographics/NS-Top-Twenty-Baby-Names-1920-2016/emf8-vmuy
 
 
@@ -13,23 +6,33 @@ library(shiny)
 library(readr)
 
 
-babynames <- read_csv("~/Sites/nsbabynames/NS_Top_Twenty_Baby_Names_-_1920-2016.csv", col_types = cols(
+babynames <- read_csv(paste0(getwd(), "/NS_Top_Twenty_Baby_Names_-_1920-2016.csv"), col_types = cols(
   sex = col_factor(levels = c("M", "F")),
   year = col_datetime(format = "%m/%d/%Y %H:%M:%S %p"),
   count = col_integer()))
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
-  # browser(babynames)
+  years <- format(babynames[['year']],'%Y')
+  firstYear <- as.numeric(years[1])
+  lastYear <- as.numeric(years[length(years)])
+  
+  output$yearSlider <- renderUI({
+    sliderInput("year",
+      "Year:",
+      min = firstYear,
+      max = lastYear,
+      value = firstYear)
+  })
+  
+  browser()
+  
+  yearPlot <- reactive({
+    currentYear <- input$year
+    
+  })
+  
   output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    cnt <- gsub(",", "", babynames['count'])
-    cnt <- as.numeric(cnt)
-    
-    
-    renderPlot({
-      plot(c(1, 2, 4, 5, 6), c(6, 7, 8, 9, 10))
-    })
+    plot(babynames[['count']], format(babynames[['year']],'%Y'))
   })
 })
