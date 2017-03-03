af736acf2abd7d51361b9c1fd13564abbebbe4d9
Gavin Anderegg <gavin@anderegg.ca>
2017-03-03 13:02:30 -0400

    Adding colour

--- server.R
+++ server.R
@@ -68,7 +68,7 @@ shinyServer(function(input, output) {
     outputVals <- subset(babynames, year == inputYear & sex == inputGender)
     outputVals$`first name` <- factor(outputVals$`first name`, levels=unique(as.character(outputVals$`first name`)))
     
-    p <- ggplot(aes(x=`first name`, y=count), data=outputVals) + geom_bar(stat="identity") + coord_flip() + 
+    p <- ggplot(aes(x=`first name`, y=count, fill=count), data=outputVals) + geom_bar(stat="identity") + coord_flip() + 
 scale_x_discrete(limits = rev(levels(outputVals$`first name`)))
     print(p)
   })
@@ -83,7 +83,7 @@ scale_x_discrete(limits = rev(levels(outputVals$`first name`)))
     
     outputVals <- subset(babynames, `first name` == inputName)
     
-    p <- ggplot(aes(x=year, y=count, group=1), data=outputVals) + geom_line() + geom_point()
+    p <- ggplot(aes(x=year, y=count, group=1, color=count), data=outputVals) + geom_line() + geom_point()
     print(p)
   })
   
